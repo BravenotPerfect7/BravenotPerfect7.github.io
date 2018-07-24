ccc53672feb1d6bc105adf8f4d1863c5ee1a7c52
unknown <marianam@bronxparkms.info>
2018-07-24 15:20:01 -0400

    My first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("Hi")
+<html>
+<body>
+
+<h2>JavaScript Statements</h2>
+
+<p>In HTML, JavaScript statements are executed by the browser.</p>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = "Hello!";
+</script>
+
+</body>
+</html>
